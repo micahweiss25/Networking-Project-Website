b00110dd75a847f645289d9163df827cd14a0962
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-10-23 14:56:03 -0400

    big one

--- pr2.py
+++ pr2.py
@@ -9,17 +9,28 @@ global dir_list
 cur_path = os.getcwd()
 dir_list = os.listdir(cur_path)
 
+global moved_dict
+with open("moved", "r") as f:
+    moved_dict = {y[0]: y[1] for y in [x.split(" ") for x in f.read().split("\n")]}
+    print(moved_dict)
+
+
 
 global lastModified
 lastModified = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
+
+
+
 # https://picamera.readthedocs.io/en/release-1.13/recipes2.html#web-streaming I used this web page that I found when working on a side project. I used the code to figure out how to format the get request.
 # https://docs.python.org/3/library/http.server.html I don't think I have to site python docs, but I used this site ase well
 # https://www.life.illinois.edu/edtech/html/styles/ I used this link to figure out static css formating
+# https://www.w3schools.com/html/default.asp
+
 class MyServer(server.BaseHTTPRequestHandler):
     def do_HEAD(self):
         ts = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
         if self.path == '/':
-            with open('templates/index.html', 'rb') as f:
+            with open('templates/windows.html', 'rb') as f:
                 self.custom_headers(ts, f.read(), 200, "text/html", False)
         else:
             try:
@@ -27,18 +38,18 @@ class MyServer(server.BaseHTTPRequestHandler):
                     ext = os.path.splitext(self.path.split("/")[-1])[1]
                     self.custom_headers(ts, f.read(), 200, ext, False)
             except:
-                with open("moved", "r") as f:
-                    if self.path in f:
-                        self.send_error(301, "Moved Permanently")
-                    else:
-                        self.send_error(404, "File Not Found")
+                if self.path[1:] in list(moved_dict.keys()):
+                    self.send_error(301, "Moved Permanently")
+                    self.send_header("Location", moved_dict[self.path[1:]])
+                else:
+                    self.send_error(404, "File Not Found")
 
     def do_GET(self):
         print(cur_path)
         ts = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
         print(f"file path: {self.path}")
         if self.path == '/':
-            with open('templates/index.html', 'rb') as f:
+            with open('templates/windows.html', 'rb') as f:
                 self.custom_headers(ts, f.read(), 200, ".html", True)
         else:
             try:
@@ -46,11 +57,11 @@ class MyServer(server.BaseHTTPRequestHandler):
                     ext = os.path.splitext(self.path.split("/")[-1])[1]
                     self.custom_headers(ts, f.read(), 200, ext, True)
             except:
-                with open("moved", "r") as f:
-                    if self.path in f:
-                        self.send_error(301, "Moved Permanently")
-                    else:
-                        self.send_error(404, "File Not Found")
+                if self.path[1:] in list(moved_dict.keys()):
+                    self.send_error(301, "Moved Permanently")
+                    self.send_header("Location", moved_dict[self.path[1:]])
+                else:
+                    self.send_error(404, "File Not Found")
 
     def custom_headers(self, ts, webFile, response, content_type, get):
         if "If-Modified-Since" in self.headers and self.headers["If-Modified-Since"] == ts:
@@ -66,6 +77,8 @@ class MyServer(server.BaseHTTPRequestHandler):
                 self.send_header("Content-type", "image/gif")
             elif content_type == ".html":
                 self.send_header("Content-type", "text/html")
+            elif content_type == ".mp4":
+                self.send_header("Content-type", "video/mp4")
             else:
                 self.send_error(404, "unknown extension")
             #self.send_header("Date", ts)
